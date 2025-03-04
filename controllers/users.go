2e897d6ea8fb8aaf5bf6437698db2239ff07eccb
s_prime <s_prime@debian12.com>
2025-03-04 19:01:30 +0200

    parse the signup form values

--- controllers/users.go
+++ controllers/users.go
@@ -1,9 +1,12 @@
 package controllers
 
 import (
+	"fmt"
 	"net/http"
 )
 
+// the Template type is an interface I define
+// in controllers/template.go
 type Users struct {
 	Templates struct {
 		New Template
@@ -12,4 +15,11 @@ type Users struct {
 
 func (u Users) New(w http.ResponseWriter, r *http.Request) {
 	u.Templates.New.Execute(w, nil)
+}
+
+func (u Users) Create(w http.ResponseWriter, r *http.Request) {
+	// FormValue calls r.ParseForm first so I do not need
+	// to call it separately
+	fmt.Fprint(w, "Email: ", r.FormValue("email"))
+	fmt.Fprint(w, "Password: ", r.FormValue("password"))
 }
--- main.go
+++ main.go
@@ -37,6 +37,7 @@ func main() {
 		"signup.html", "tailwind.html",
 	))
 	r.Get("/signup", usersC.New)
+	r.Post("/users", usersC.Create)
 
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Page not found", http.StatusNotFound)
