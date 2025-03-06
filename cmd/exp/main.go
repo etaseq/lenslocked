be003ef6afbdb65adcc2bddd7fdeaa7cac8b6333
s_prime <s_prime@debian12.com>
2025-03-06 16:33:24 +0200

    use QueryRow for quering rows and inserting rows when I want to get back the id of the new enrty

--- cmd/exp/main.go
+++ cmd/exp/main.go
@@ -62,17 +62,58 @@ func main() {
 	}
 	fmt.Println("Tables created.")
 
+	//##############################################################
 	// Insert some data ...
 	// notice that $1, $2 act as placeholders
-	name := "Jon Calhoun"
-	email := "jon@calhoun.io"
+	//name := "Jon Calhoun"
+	//email := "jon@calhoun.io"
 
-	_, err = db.Exec(`
-    INSERT INTO users (name, email)
-    VALUES ($1, $2);`, name, email)
+	//_, err = db.Exec(`
+	//  INSERT INTO users (name, email)
+	//  VALUES ($1, $2);`, name, email)
+	//if err != nil {
+	//	panic(err)
+	//}
+
+	//fmt.Println("User created.")
+
+	//###############################################################
+	// Insert data and get the id back.
+	// This cannot be done with Exec but we can use QueryRow
+	//name := "Mike Bisbin"
+	//email := "mike@bisbin.com"
+
+	//row := db.QueryRow(`
+	//  INSERT INTO users (name, email)
+	//  VALUES ($1, $2) RETURNING id;`, name, email)
+
+	//var id int
+	//// Remember in Go, function arguments are always passed by value
+	//// (a copy of the original). So if you want to modify a variable,
+	//// you must pass a pointer to that variable.
+	//err = row.Scan(&id)
+
+	//if err != nil {
+	//	panic(err)
+	//}
+	//fmt.Println("User created. id = ", id)
+
+	//##############################################################
+
+	// Query data with QueryRow
+	id := 2
+	row := db.QueryRow(`
+    SELECT name, email
+    FROM users
+    WHERE id=$1;`, id)
+
+	var name, email string
+	err = row.Scan(&name, &email)
+	if err == sql.ErrNoRows {
+		fmt.Println("Error, no rows")
+	}
 	if err != nil {
 		panic(err)
 	}
-
-	fmt.Println("User created.")
+	fmt.Printf("User information: name=%s, email=%s\n", name, email)
 }
