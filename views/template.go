f6a688c505c5c22cffd1c6643006cceb3c3a5df1
s_prime <s_prime@debian12.com>
2025-03-20 16:24:46 +0200

    implement template function errors

--- views/template.go
+++ views/template.go
@@ -1,8 +1,10 @@
 package views
 
 import (
+	"bytes"
 	"fmt"
 	"html/template"
+	"io"
 	"io/fs"
 	"log"
 	"net/http"
@@ -32,12 +34,17 @@ func ParseFS(fs fs.FS, patterns ...string) (Template, error) {
 	// when the template is parsed doesn't return an error.
 	tpl = tpl.Funcs(
 		template.FuncMap{
-			"csrfField": func() template.HTML {
-				return `<!-- TODO: Implement the csrfField -->`
+			"csrfField": func() (template.HTML, error) {
+				return "", fmt.Errorf("csrfField not implemented")
 			},
 		},
 	)
 
+	// Always define your template functions first
+	// and then parse the template afterward. If you try to parse
+	// the template first and this template happens to have the
+	// field {{ csrfField }} it will throw an error because
+	// it cannot find the template function.
 	tpl, err := tpl.ParseFS(fs, patterns...)
 	if err != nil {
 		return Template{}, fmt.Errorf("parsing template: %w", err)
@@ -46,17 +53,6 @@ func ParseFS(fs fs.FS, patterns ...string) (Template, error) {
 		htmlTpl: tpl,
 	}, nil
 }
-
-//func Parse(filepath string) (Template, error) {
-//	tpl, err := template.ParseFiles(filepath)
-//	if err != nil {
-//		return Template{}, fmt.Errorf("parsing template: %w", err)
-//	}
-//
-//	return Template{
-//		htmlTpl: tpl,
-//	}, nil
-//}
 
 func (t Template) Execute(w http.ResponseWriter, r *http.Request, data interface{}) {
 	tpl, err := t.htmlTpl.Clone()
@@ -74,11 +70,18 @@ func (t Template) Execute(w http.ResponseWriter, r *http.Request, data interface
 	)
 
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	err = tpl.Execute(w, data)
-
+	// Make sure that the page is fully rendered before send it to the writer.
+	// Keep in mind that if you have a massive page this might cause performance
+	// issues. So you should weight whether or not in case of an error you
+	// would prefer a half rendered page or a slower rendering.
+	// If you have pages that have a couple MBs of HTML rethink to use a buffer.
+	var buf bytes.Buffer
+	err = tpl.Execute(&buf, data)
 	if err != nil {
 		log.Printf("executing template: %v", err)
 		http.Error(w, "There was an error executing the template.", http.StatusInternalServerError)
 		return
 	}
+
+	io.Copy(w, &buf)
 }
