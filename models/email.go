d76fdf12500586685b4ec65391b1bd7944392b3e
s_prime <s_prime@debian12.com>
2025-04-08 18:50:28 +0300

    add the NewEmailService constructor and Send, SetFrom methods

--- models/email.go
+++ models/email.go
@@ -1,10 +1,29 @@
 package models
 
-import "github.com/go-mail/mail/v2"
+import (
+	"fmt"
+
+	"github.com/go-mail/mail/v2"
+)
 
 const (
 	DefaultSender = "support@lenslocked.com"
 )
+
+type Email struct {
+	From      string
+	To        string
+	Subject   string
+	Plaintext string
+	HTML      string
+}
+
+type SMTPConfig struct {
+	Host     string
+	Port     int
+	Username string
+	Password string
+}
 
 type EmailService struct {
 	// DefaultSender is used as the default sender when one isn't provided for an
@@ -17,13 +36,10 @@ type EmailService struct {
 	dialer *mail.Dialer
 }
 
-type SMTPConfig struct {
-	Host     string
-	Port     int
-	Username string
-	Password string
-}
-
+// Notice that I use a function to construct the EmailService type, which I
+// didn't do for the other types like UserService and SessionService.
+// Generally avoid doing that, but in cases like this one where you want
+// to pass something like a config it makes sense to use a function.
 func NewEmailService(config SMTPConfig) *EmailService {
 	es := EmailService{
 		dialer: mail.NewDialer(config.Host, config.Port,
@@ -31,4 +47,44 @@ func NewEmailService(config SMTPConfig) *EmailService {
 	}
 
 	return &es
+}
+
+func (es *EmailService) Send(email Email) error {
+	// NewMessage() returns a *mail.Message pointer
+	msg := mail.NewMessage()
+	msg.SetHeader("To", email.To)
+	es.setFrom(msg, email)
+	msg.SetHeader("Subject", email.Subject)
+
+	switch {
+	case email.Plaintext != "" && email.HTML != "":
+		msg.SetBody("text/plain", email.Plaintext)
+		msg.AddAlternative("text/html", email.HTML)
+	case email.Plaintext != "":
+		msg.SetBody("text/plain", email.Plaintext)
+	case email.HTML != "":
+		msg.SetBody("text/html", email.HTML)
+	}
+
+	err := es.dialer.DialAndSend(msg)
+	if err != nil {
+		return fmt.Errorf("send: %w", err)
+	}
+
+	return nil
+}
+
+func (es *EmailService) setFrom(msg *mail.Message, email Email) {
+	var from string
+
+	switch {
+	case email.From != "":
+		from = email.From
+	case es.DefaultSender != "":
+		from = es.DefaultSender
+	default:
+		from = DefaultSender
+	}
+
+	msg.SetHeader("From", from)
 }
