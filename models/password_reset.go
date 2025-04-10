693b525ba9f536364a228adc834aa551420651aa
s_prime <s_prime@debian12.com>
2025-04-10 16:50:56 +0300

    finish the PasswordResetService Consume method

--- models/password_reset.go
+++ models/password_reset.go
@@ -89,7 +89,40 @@ func (service *PasswordResetService) Create(email string) (*PasswordReset, error
 }
 
 func (service *PasswordResetService) Consume(token string) (*User, error) {
-	return nil, fmt.Errorf("TODO: Implement PasswordResetService.Consume")
+	tokenHash := service.hash(token)
+	var user User
+	var pwReset PasswordReset
+
+	// First check that I have a valid PasswordReset.
+	row := service.DB.QueryRow(`
+		SELECT password_resets.id, 
+			password_resets.expires_at,
+			users.id,
+			users.email,
+			users.password_hash
+		FROM password_resets
+		JOIN users ON users.id = password_resets.users_id
+		WHERE password_resets.token_hash = $1;`, tokenHash)
+	err := row.Scan(
+		&pwReset.ID, &pwReset.ExpiresAt, &user.ID,
+		&user.Email, &user.PasswordHash)
+
+	if err != nil {
+		return nil, fmt.Errorf("consume: %w", err)
+	}
+
+	// Next check if the PasswordReset has not expired.
+	if time.Now().After(pwReset.ExpiresAt) {
+		return nil, fmt.Errorf("token expired: %v", token)
+	}
+
+	// Consume the PasswordReset token
+	err = service.delete(pwReset.ID)
+	if err != nil {
+		return nil, fmt.Errorf("consume: %w", err)
+	}
+
+	return &user, nil
 }
 
 func (service *PasswordResetService) hash(token string) string {
@@ -98,4 +131,15 @@ func (service *PasswordResetService) hash(token string) string {
 	// to convert the tokenHash array to a slice to feed it to
 	// EncodeToString
 	return base64.URLEncoding.EncodeToString(tokenHash[:])
+}
+
+func (service *PasswordResetService) delete(id int) error {
+	_, err := service.DB.Exec(`
+		DELETE FROM password_resets
+		WHERE id = $1;`, id)
+	if err != nil {
+		return fmt.Errorf("delete: %w", err)
+	}
+
+	return nil
 }
