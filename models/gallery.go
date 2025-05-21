3707bdc42fb691a614252753235318e07714f367
s_prime <s_prime@debian12.com>
2025-05-21 12:09:49 +0300

    add the Create and ByID methods in the GalleryService to create and search for a gallery

--- models/gallery.go
+++ models/gallery.go
@@ -1,7 +1,55 @@
 package models
 
+import (
+	"database/sql"
+	"fmt"
+)
+
 type Gallery struct {
 	ID     int
 	UserID int
 	Title  string
+}
+
+type GalleryService struct {
+	DB *sql.DB
+}
+
+func (service *GalleryService) Create(title string, userID int) (*Gallery, error) {
+	// TODO: Add validation for the id (no need for an MVP)
+	gallery := Gallery{
+		Title:  title,
+		UserID: userID,
+	}
+	row := service.DB.QueryRow(`
+		INSERT INTO galleries (title, user_id)
+		VALUES ($1, $2) RETURNING id;`, gallery.Title, gallery.UserID)
+	err := row.Scan(&gallery.ID)
+
+	// I don't need to look for unique violations since two galleries can
+	// have the same title and a user can have multiple galleries.
+	if err != nil {
+		return nil, fmt.Errorf("create gallery: %w", err)
+	}
+
+	return &gallery, nil
+}
+
+func (service *GalleryService) ByID(id int) (*Gallery, error) {
+	// TODO: Add validation for the id (no need for an MVP)
+	gallery := Gallery{
+		ID: id,
+	}
+
+	row := service.DB.QueryRow(`
+		SELECT title, user_id
+		FROM galleries
+		WHERE id = $1;`, gallery.ID)
+	err := row.Scan(&gallery.Title, &gallery.UserID)
+
+	if err != nil {
+		return nil, fmt.Errorf("query gallery by id: %w", err)
+	}
+
+	return &gallery, nil
 }
