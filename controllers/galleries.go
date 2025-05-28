f01941cccb672298d8cd3ac162a07b4c1547a0b8
s_prime <s_prime@debian12.com>
2025-05-28 16:20:53 +0300

    add the Create handler in the galleries controller

--- controllers/galleries.go
+++ controllers/galleries.go
@@ -1,8 +1,10 @@
 package controllers
 
 import (
+	"fmt"
 	"net/http"
 
+	"github.com/etaseq/lenslocked/context"
 	"github.com/etaseq/lenslocked/models"
 )
 
@@ -20,4 +22,25 @@ func (g Galleries) New(w http.ResponseWriter, r *http.Request) {
 
 	data.Title = r.FormValue("title")
 	g.Templates.New.Execute(w, r, data)
+}
+
+func (g Galleries) Create(w http.ResponseWriter, r *http.Request) {
+	var data struct {
+		UserID int
+		Title  string
+	}
+	// I assume that the user is signed in because in the routes I
+	// use the RequireUser middleware. This is the reason I use
+	// context without even checking if the user is nil.
+	data.UserID = context.User(r.Context()).ID
+	data.Title = r.FormValue("title")
+
+	gallery, err := g.GalleryService.Create(data.Title, data.UserID)
+	if err != nil {
+		g.Templates.New.Execute(w, r, data, err)
+		return
+	}
+
+	editPath := fmt.Sprintf("/galleries/%d/edit", gallery.ID)
+	http.Redirect(w, r, editPath, http.StatusFound)
 }
